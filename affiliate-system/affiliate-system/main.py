5adb7b1d9ecd481dd9712e698c61893f1bfdd5fe
andreigsilva-hub <andrei.gsilva@fisia.com.br>
2026-05-20 22:49:00 -0300

    Update main.py

--- affiliate-system/affiliate-system/main.py
+++ affiliate-system/affiliate-system/main.py
@@ -1,1 +1,17 @@
+from fastapi import FastAPI
+from database import Base, engine
 
+from routes.users import router as users_router
+from routes.offers import router as offers_router
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(users_router)
+app.include_router(offers_router)
+
+
+@app.get("/")
+def home():
+    return {"status": "rodando"}
